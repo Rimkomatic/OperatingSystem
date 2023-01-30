ff5faa6696940b5d4bd869875291aa6163806530
Rik Mahapatra <96734674+W41K3R0@users.noreply.github.com>
2023-01-30 21:22:41 +0530

    Added the '%' operation in the code

--- lab3/program1.sh
+++ lab3/program1.sh
@@ -6,9 +6,11 @@ c=`expr $a + $b `
 d=`expr $a - $b `
 e=`expr $a \* $b `
 f=`expr $a / $b `
+g=`expr $a % $b `
+
 echo "The + operation is $c"
 echo "The - operation is $d"
 echo "The * operation is $e"
 echo "The / operation is $f"
-
+echo "The % operation is $g"
 
